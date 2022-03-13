2175089bbacc30938f087702d0165093c0af8e22
KaviiSuri <surikavii@gmail.com>
2022-03-13 19:20:57 +0530

    moved arithmetic functions to different files

---
+++ linalg/arithmetic.go
@@ -0,0 +1,61 @@
+package linalg
+
+import "log"
+
+func Add(v, o Vec) Vec {
+	return NewFromEachPair(v, o, func(a, b float64) float64 {
+		return a + b
+	})
+}
+
+func Sub(v, o Vec) Vec {
+	return NewFromEachPair(v, o, func(a, b float64) float64 {
+		return a + b
+	})
+}
+func Mul(v, o Vec) Vec {
+	return NewFromEachPair(v, o, func(a, b float64) float64 {
+		return a * b
+	})
+}
+
+func Divide(v, o Vec) Vec {
+	return NewFromEachPair(v, o, func(a, b float64) float64 {
+		return a / b
+	})
+}
+
+func Max(v, o Vec) Vec {
+	return NewFromEachPair(v, o, func(a, b float64) float64 {
+		if a > b {
+			return a
+		} else {
+			return b
+		}
+	})
+}
+
+func Min(v, o Vec) Vec {
+	return NewFromEachPair(v, o, func(a, b float64) float64 {
+		if a < b {
+			return a
+		} else {
+			return b
+		}
+	})
+}
+
+func Lerp(v, o, t Vec) Vec {
+	checkEqualSize(v, o)
+	return Map(v, func(a float64, i int) float64 {
+		return a + (o.Values[i]-a)*t.Values[i]
+	})
+}
+
+func checkEqualSize(v Vec, vectors ...Vec) {
+	for _, o := range vectors {
+		if v.Size != o.Size {
+			log.Fatalf("Vectors Should be of the same size: %v != %v", v.Size, o.Size)
+		}
+	}
+}
--- linalg/vec.go
+++ linalg/vec.go
@@ -1,7 +1,6 @@
 package linalg
 
 import (
-	"log"
 	"math"
 )
 
@@ -62,66 +61,6 @@ func NewFromEachPair(v Vec, o Vec, f func(a, b float64) float64) Vec {
 	return Map(v, func(a float64, i int) float64 {
 		return f(a, o.Values[i])
 	})
-}
-
-// Functions
-
-func Add(v, o Vec) Vec {
-	return NewFromEachPair(v, o, func(a, b float64) float64 {
-		return a + b
-	})
-}
-
-func Sub(v, o Vec) Vec {
-	return NewFromEachPair(v, o, func(a, b float64) float64 {
-		return a + b
-	})
-}
-func Mul(v, o Vec) Vec {
-	return NewFromEachPair(v, o, func(a, b float64) float64 {
-		return a * b
-	})
-}
-
-func Divide(v, o Vec) Vec {
-	return NewFromEachPair(v, o, func(a, b float64) float64 {
-		return a / b
-	})
-}
-
-func Max(v, o Vec) Vec {
-	return NewFromEachPair(v, o, func(a, b float64) float64 {
-		if a > b {
-			return a
-		} else {
-			return b
-		}
-	})
-}
-
-func Min(v, o Vec) Vec {
-	return NewFromEachPair(v, o, func(a, b float64) float64 {
-		if a < b {
-			return a
-		} else {
-			return b
-		}
-	})
-}
-
-func Lerp(v, o, t Vec) Vec {
-	checkEqualSize(v, o)
-	return Map(v, func(a float64, i int) float64 {
-		return a + (o.Values[i]-a)*t.Values[i]
-	})
-}
-
-func checkEqualSize(v Vec, vectors ...Vec) {
-	for _, o := range vectors {
-		if v.Size != o.Size {
-			log.Fatalf("Vectors Should be of the same size: %v != %v", v.Size, o.Size)
-		}
-	}
 }
 
 // Accessors Aliases
