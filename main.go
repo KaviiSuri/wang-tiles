21d1437111f908c1e1d9e319e5247175b0e42b27
KaviiSuri <surikavii@gmail.com>
2022-03-14 22:55:40 +0530

    added grid and atlas to results

--- main.go
+++ main.go
@@ -41,9 +41,7 @@ func circle(uv linalg.Vec) color.Normalized {
 
 func main() {
 	filename := "./temp/grid.jpeg"
-	if len(os.Args) > 1 {
-		filename = os.Args[1]
-	}
+	atlasFile := "./temp/atlas.jpeg"
 	f, err := os.Create(filename)
 	if err != nil {
 		log.Fatal(err)
@@ -51,12 +49,18 @@ func main() {
 	defer f.Close()
 	tileSet := wang.NewWangTileSet(tileWidth, tileHeight)
 	img := wang.NewWangGrid(tileSet, widthInTiles, heightInTiles, tileWidth, tileHeight)
-	//bltrs := [][]uint8{
-	//{0, 1, 2, 3},
-	//{4, 5, 6, 7},
-	//{8, 9, 10, 11},
-	//{12, 13, 14, 15},
-	//}
-	//img := wang.NewGridFromBLTRS(bltrs, tileSet, tileSetNumTilesH, tileSetNumTilesV, tileWidth, tileHeight)
 	jpeg.Encode(f, img, nil)
+	bltrs := [][]uint8{
+		{0, 1, 2, 3},
+		{4, 5, 6, 7},
+		{8, 9, 10, 11},
+		{12, 13, 14, 15},
+	}
+	img = wang.NewGridFromBLTRS(bltrs, tileSet, 4, 4, tileWidth, tileHeight)
+	af, err := os.Create(atlasFile)
+	defer af.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	jpeg.Encode(af, img, nil)
 }
